cd152588f4ab1956129e0a59145353dee7947f5b
Sasitilak <116880437+Sasitilak@users.noreply.github.com>
2024-05-11 21:45:53 +0530

    Update BlogAPP.py

--- BlogAPP.py
+++ BlogAPP.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import wikipedia
 from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os 
@@ -9,28 +8,16 @@ import os
 load_dotenv()
 
 # API key for accessing Groq Langchain service
-groq_api_key = 'GROQ_API_KEY'
+groq_api_key = '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'
 
 def main():
     st.title("Blog Generation System")
 
     # Add customization options to the sidebar
     model = 'mixtral-8x7b-32768'  # Model for language generation
-    conversational_memory_length = 10  # Length of conversational memory
-
-    # Initialize conversational memory
-    memory = ConversationBufferWindowMemory(k=conversational_memory_length)
 
     # Text area for user to input blog topic
     user_question = st.text_area("Give Blog Topic")
-
-    # session state variable to store chat history
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = []
-    else:
-        # Load chat history into memory
-        for message in st.session_state.chat_history:
-            memory.save_context({'input': message['human']}, {'output': message['AI']})
 
     # Initialize Groq Langchain chat object and conversation
     groq_chat = ChatGroq(
@@ -39,8 +26,7 @@ def main():
     )
 
     conversation = ConversationChain(
-        llm=groq_chat,
-        memory=memory
+        llm=groq_chat
     )
 
     # Button to trigger blog post generation
@@ -58,8 +44,6 @@ def main():
                 # If Wikipedia page not found, generate blog post directly using the topic
                 input_with_reference = f"Create a blog with the Heading, Introduction, context(3 paragraphs), summary using the topic \"{topic}\"."
                 response = conversation(input_with_reference)
-                message = {'human': user_question, 'AI': response['response']}
-                st.session_state.chat_history.append(message)
                 st.write("Blog")
                 st.write(response['response'])
                 return
@@ -69,10 +53,8 @@ def main():
             
             # Generate blog post using conversation chain
             response = conversation(input_with_reference)
-            message = {'human': user_question, 'AI': response['response']}
             
-            # Append user input and AI response to chat history
-            st.session_state.chat_history.append(message)
+            # Display the generated blog post
             st.write("Blog")
             st.write(response['response'])
 
